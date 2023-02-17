6c05daf68f14da05a242ff12bb134ca5259fec6a
MbaliFelecia <ramakgashambali@gmail.com>
2023-02-16 18:01:53 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,7 +6,8 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    return (1);
+	write(2, str1, 59);
+	return (1);
 }
