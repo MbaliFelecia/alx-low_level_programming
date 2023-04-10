57c1dda95ca16cd11675ccccb06b6296d5b55de3
MbaliFelecia <ramakgashambali@gmail.com>
2023-04-09 23:07:49 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_
 #define _MAIN_
 
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
