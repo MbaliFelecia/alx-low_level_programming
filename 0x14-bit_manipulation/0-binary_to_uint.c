d0e38652cf6ae8892a8a977acf4455dc333415de
MbaliFelecia <ramakgashambali@gmail.com>
2023-04-10 01:52:50 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,30 +8,30 @@
  * Return: unsigned int .
  */
 unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int j, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (j = 0; b[j] != '\0'; j++)
+		;
+
+	for (j--, base_two = 1; j >= 0; j--, base_two *= 2)
 	{
-		unsigned int ui;
-		int j, base_two;
-
-		if (!b)
-			return (0);
-
-		ui = 0;
-
-		for (j = 0; b[j] != '\0'; j++)
-			;
-
-		for (j--, base_two = 1; j >= 0; j--, base_two *= 2)
+		if (b[j] != '0' && b[j] != '1')
 		{
-			if (b[j] != '0' && b[j] != '1')
-			{
-				return (0);
-			}
-
-			if (b[j] & 1)
-			{
-				ui += base_two;
-			}
+			return (0);
 		}
 
-		return (ui);
+		if (b[j] & 1)
+		{
+			ui += base_two;
+		}
 	}
+
+		return (ui);
+}
