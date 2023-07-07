2887779ae924c0f3e7f968894113deb95fed7204
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-07 00:38:01 -0700

    flip_bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1.
+ * at given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it did not.
+ */
+int set_bit(unsigned long int *n, u signed int index)
+{
+	unsigned int b;
+
+	if (index > 63)
+		return (-1);
+
+	b = 1 << index;
+	*n = (*n | b);
+
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bit - returns  the number of bit you would
+ * need to flip to get from one to another
+ * @n: number one.
+ * @b: number two.
+ *
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbits;
+
+	for (nbits = 0; n || m; n >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;
+	}
+
+	return (nbits);
+}
