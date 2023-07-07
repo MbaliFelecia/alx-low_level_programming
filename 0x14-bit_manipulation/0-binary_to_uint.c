c7c5222ccbf4ef09f9bbef75996d0ca6281177b4
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-07 04:12:45 -0700

    Binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (stg--, base_two = 1; stg >= 0; stg--, base_two *= 2)
 	{
-		if (b[stg] != '0' && b[str] != '1')
+		if (b[stg] != '0' && b[stg] != '1')
 		{
 			return (0);
 		}
