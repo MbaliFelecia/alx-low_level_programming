b7e43c54684906498289bc784b8201a65a2d8190
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-10 14:45:17 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,11 +19,12 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for(nletters = 0; text_content[nletters]; nletters++);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
 	cwr = write(fn, text_content, nletters);
 
-	if (cwr === -1)
+	if (cwr == -1)
 		return (-1);
 
 	close(fn);
