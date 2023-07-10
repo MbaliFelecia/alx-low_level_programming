b8ee1188187e18debe5d4b7a8f22832fc84c787e
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-10 15:07:32 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,6 +5,7 @@
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
+ * letters: numbers of letters printed.
  *
  * Return: numbers of letters printed. It fails, return 0.
  */
@@ -21,12 +22,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fl = open(filename, O_RDONLY);
 
 	if (fl == -1)
-		return(0);
+		return (0);
 
 	temp = malloc(letters);
 	if (!temp)
 		return (0);
-	
 	num_char = read(fl, temp, letters);
 	wr = write(STDOUT_FILENO, temp, num_char);
 
@@ -35,4 +35,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(temp);
 
 	return (num_char);
-} 
+}
