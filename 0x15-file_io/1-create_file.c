952aaff1c4dc9efb7c167fec2d2a4f566c15249a
MbaliFelecia <ramakgashambali@gmail.com>
2023-07-10 15:58:58 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * create_file - creates a file
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fn = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (!text_content)
 		text_content = "";
