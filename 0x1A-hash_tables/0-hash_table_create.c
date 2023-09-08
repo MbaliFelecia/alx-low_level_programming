3cb77d26b82616e55695d4a8f813cfc16d392e7e
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-07 23:18:53 -0700

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
-	table = malloc(sizeof(hast_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
